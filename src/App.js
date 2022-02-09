09da11dd1f64471d40cdbb61ea23c5beaeebbfdc
Your Name <you@example.com>
2022-02-09 12:30:03 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -7,35 +7,35 @@ function App() {
   const [quote, setQuote] = useState('Loading...');
 
 //Axios
-// useEffect(() => {
-//     const interval = setInterval(() => {
-//        let urlFinal = "https://api.kanye.rest";
-//        axios.get(urlFinal)
-//        .then((res) => {
-//          //Get Quote From URL
-//          let newQoute = res.data.quote;
-//          setQuote(newQoute);
-//      })
-//            setUrl(urlFinal);
-//        }, 5000);
-//        return () => clearInterval(interval);
-// }, [url]);
+useEffect(() => {
+    const interval = setInterval(() => {
+       let urlFinal = "https://api.kanye.rest";
+       axios.get(urlFinal)
+       .then((res) => {
+         //Get Quote From URL
+         let newQoute = res.data.quote;
+         setQuote(newQoute);
+     })
+           setUrl(urlFinal);
+       }, 5000);
+       return () => clearInterval(interval);
+}, [url]);
 
 //Fetch API
-useEffect(() => {
-  const interval = setInterval(() => {
-      let urlFinal = "https://api.kanye.rest";
-      fetch(urlFinal)
-      .then(response => response.json())
-      .then (json => {
-      let newQoute = json.quote;
-      setQuote(newQoute);
-   })
-    .catch(error => console.log("There was a problem loading a Kanye quote, please try again later"))
-    setUrl(urlFinal);
-  }, 1000);
-  return () => clearInterval(interval);
-  }, [url]);
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//       let urlFinal = "https://api.kanye.rest";
+//       fetch(urlFinal)
+//       .then(response => response.json())
+//       .then (json => {
+//       let newQoute = json.quote;
+//       setQuote(newQoute);
+//    })
+//     .catch(error => console.log("There was a problem loading a Kanye quote, please try again later"))
+//     setUrl(urlFinal);
+//   }, 1000);
+//   return () => clearInterval(interval);
+//   }, [url]);
 
   return (
     <div className="App">
